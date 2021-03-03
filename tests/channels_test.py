53b950bad46b58f3f0916ab7485373c0a4bd0cb9
Gio Ko <z5310479@ad.unsw.edu.au>
2021-03-03 12:58:05 +1100

    Added channels_test.py. Added one test function.

---
+++ tests/channels_test.py
@@ -0,0 +1,10 @@
+import pytest
+
+from src.channels import channels_create_v1
+from src.error import InputError
+
+def test_channels_create_invalid_channel_name():
+    with pytest.raises(InputError):
+        assert channels_create_v1(1, Nameismorethan20characters, 1)
+
+
