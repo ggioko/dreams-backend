955446c81c241e39c00b1310dab19c0a601960dd
Sundeep Sarwana <z5208459@student.unsw.edu.au>
2021-03-03 18:11:43 +1100

    Added a framework for auth_tests

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -5,5 +5,20 @@ from src.auth import auth_register_v1
 from src.error import InputError
 
 
-def test_auth_register():
+def test_auth_register_valid():
+    pass
+
+def test_auth_register_invalid_email():
+    pass
+
+def test_auth_register_taken_email():
+    pass
+
+def test_auth_register_invalid_password():
+    pass
+
+def test_auth_register_invalid_firstname():
+    pass
+
+def test_auth_register_invalid_lastname():
     pass
