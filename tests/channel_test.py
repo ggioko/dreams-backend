2469a5b3563361fdc5625e2bc5260aa09ef13a6f
Ben Rayner <z5256104@ad.unsw.edu.au>
2021-03-03 18:24:42 +1100

    Test stubs for functions implemented

--- tests/channel_test.py
+++ tests/channel_test.py
@@ -8,8 +8,7 @@ def test_channel_invite_invalid_channel():
     pass
 
 def test_channel_invite_invalid_user():
-    with pytest.raises(InputError):
-        assert channel_invite_v1(1, 1, -5)
+    pass
 
 def test_channel_invite_invalid_authoriser():
     pass
