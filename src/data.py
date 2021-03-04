8537f32c0e51c70cf155528eb521789f14c157bd
Sundeep Sarwana <z5208459@student.unsw.edu.au>
2021-03-04 22:02:46 +1100

    Created a file to store data

---
+++ src/data.py
@@ -0,0 +1,22 @@
+# Data storage global variable
+data = {
+    'users': [
+        {
+            'u_id': 1,
+            'email': 'cs1531@cse.unsw.edu.au',
+            'name_first': 'Hayden',
+            'name_last': 'Jacobs',
+            'handle_str': 'haydenjacobs',
+        },
+    ],
+    'channels': [
+        {
+            'id': 1,
+            'name' : 'channel1',
+        },
+        {
+            'id': 2,
+            'name' : 'channel2',
+        },
+    ],
+}
