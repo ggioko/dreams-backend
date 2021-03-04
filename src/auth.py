5f985a6b9156730539fe9263f6b9eba477d05bfa
Sundeep Sarwana <z5208459@student.unsw.edu.au>
2021-03-04 23:14:38 +1100

    Fixed index issue when data was empty

--- src/auth.py
+++ src/auth.py
@@ -31,10 +31,36 @@ def auth_login_v1(email, password):
 
 
 def auth_register_v1(email, password, name_first, name_last):
-    # Email in use check
-    emails = [data['users'][c]['email'] for c in range(len(data['users']))]
-    if email in emails:
-        raise InputError('Email already in use')
+    # Check if users data is empty
+    if len(data['users']) != 0:
+        # Email in use check
+        emails = [data['users'][c]['email'] for c in range(len(data['users']))]
+        if email in emails:
+            raise InputError('Email already in use')
+
+        # Gets a new id
+        ids = [data['users'][c]['u_id'] for c in range(len(data['users']))]
+        for i in range(1,len(data['users']) + 2):
+            if i not in ids:
+                id = i
+                break
+
+        # Creates a new handle
+        handle = name_first + name_last
+        handle = handle[0:20]
+        handles = [data['users'][c]['handle_str'] for c in range(len(data['users']))]
+        duplicate = True
+        count = 0
+        while duplicate:
+            if handle in handles:
+                handle += str(count)
+                count += 1
+            else:
+                duplicate = False
+    else:
+        id = 1
+        handle = name_first + name_last
+        handle = handle[0:20]
 
     # Password size check
     if len(password) < 6:
@@ -49,28 +75,7 @@ def auth_register_v1(email, password, name_first, name_last):
     # Email syntax check
     if not re.match('^[a-zA-Z0-9]+[\\._]?[a-zA-Z0-9]+[@]\\w+[.]\\w{2,3}$',email):
         raise InputError('Incorrect email format')
-
-    # Gets a new id
-    ids = [data['users'][c]['u_id'] for c in range(len(data['users']))]
-    for i in range(1,len(data['users']) + 2):
-        if i not in ids:
-            id = i
-            break
-
-    # Creates a new handle
-    handle = name_first + name_last
-    handle = handle[0:20]
-    handles = [data['users'][c]['handle_str'] for c in range(len(data['users']))]
-    duplicate = True
-    count = 0
-    while duplicate:
-        if handle in handles:
-            handle += str(count)
-            count += 1
-        else:
-            duplicate = False
-    
-
+  
     # Saves user data
     user = {
         'u_id': id,
