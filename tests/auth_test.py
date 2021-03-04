b212c93080bdd9fe992db4ad96c8c73c8dc0d534
Ben Rayner <z5256104@ad.unsw.edu.au>
2021-03-05 10:39:16 +1100

    Added variety of tests, mostly dealing with symbol input inside the email field

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -2,6 +2,7 @@ import pytest
 
 from src.auth import auth_login_v1
 from src.auth import auth_register_v1
+from src.other import clear_v1
 from src.error import InputError
 
 
@@ -9,7 +10,31 @@ def test_auth_register_valid():
     pass
 
 def test_auth_register_invalid_email():
-    pass
+    clear_v1()
+
+    password = "Pass123"
+    firstName = "John"
+    lastName = "Smith"
+    with pytest.raises(InputError) as e:
+        auth_register_v1("fold@home@stanford.edu", password, firstName, lastName)
+
+    with pytest.raises(InputError) as e:
+        auth_register_v1("hayden@coolem@ildomail.com", password, firstName, lastName)
+
+    with pytest.raises(InputError) as e:
+        auth_register_v1("@example.com", password, firstName, lastName)
+
+    with pytest.raises(InputError) as e:
+        auth_register_v1("hi@examp!e.com", password, firstName, lastName)
+
+    with pytest.raises(InputError) as e:
+        auth_register_v1("ab@~`example.com", password, firstName, lastName)
+
+    with pytest.raises(InputError) as e:
+        auth_register_v1("ab@example.!com", password, firstName, lastName)
+
+
+
 
 def test_auth_register_taken_email():
     pass
