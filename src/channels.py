ad9944126ff2bc421d98ea7bc301f0601014ad14
Sundeep Sarwana <z5208459@student.unsw.edu.au>
2021-03-05 15:12:37 +1100

    Changed function to check for access error and return correct format of list data

--- src/channels.py
+++ src/channels.py
@@ -1,4 +1,8 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from src.data import data
+from src.error import AccessError
 
 def channels_list_v1(auth_user_id):
     return {
@@ -11,9 +15,21 @@ def channels_list_v1(auth_user_id):
     }
 
 def channels_listall_v1(auth_user_id):
-    return {'channels': data['channels']}
+    ids = [data['users'][c]['u_id'] for c in range(len(data['users']))]
+    if auth_user_id not in ids:
+        raise AccessError("Invalid ID")
+    channelData = {'channels':[]}
+    for channel in data['channels']:
+        channelData['channels'].append({
+            'channel_id': channel['id'],
+        	'name': channel['name'],
+        })
+    return channelData
 
 def channels_create_v1(auth_user_id, name, is_public):
     return {
         'channel_id': 1,
     }
+
+if __name__ == "__main__":
+    print(channels_listall_v1(1))
