c00a7b60afeede3522247e5524887d2747f8917c
Ben Rayner <z5256104@ad.unsw.edu.au>
2021-03-05 13:03:16 +1100

    Written implementation for auth.py, for email checking

--- src/auth.py
+++ src/auth.py
@@ -1,9 +1,25 @@
+from src.error import InputError
+import re
+
+def isEmailValid(email):
+    if re.match('^[a-zA-Z0-9]+[\\._]?[a-zA-Z0-9]+[@]\\w+[.]\\w{2,3}$', email):
+	    return True
+    return False
+
 def auth_login_v1(email, password):
+    email = email.lower()
     return {
         'auth_user_id': 1,
     }
 
 def auth_register_v1(email, password, name_first, name_last):
+    email = email.lower()
+    if (isEmailValid(email) == False):
+        raise InputError('Invalid email format')
+
     return {
         'auth_user_id': 1,
     }
+
+if __name__ == "__main__":
+    print(auth_register_v1("fold@home@stanford.edu", "Pass123", "John", "Smith"))
