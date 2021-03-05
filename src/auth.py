911d10a9263f1eadde8f60bc2df1a36a3c0d9ffd
z5260252 <g.jayasekera@student.unsw.edu.au>
2021-03-05 13:06:44 +1100

    Basic foundation of function setup

--- src/auth.py
+++ src/auth.py
@@ -1,7 +1,21 @@
+from src.error import InputError
+from src.data import data
+import re
+
 def auth_login_v1(email, password):
-    return {
-        'auth_user_id': 1,
-    }
+    # Given a registered users' email and password and returns their `auth_user_id` value
+
+    if len(data['users']) != 0:
+        # Check if email is registered
+        email_registered = [data['users'][c]['email'] for c in range(len(data['users']))]
+        if email in email_registered:
+            # Check if the password given matches
+            password_check = [data['users'][c]['password']]
+            if password == password_check:
+                get_id = int(data['users'][c]['u_id'])
+                return {'auth_user_id': get_id}
+    else:
+        raise InputError
 
 def auth_register_v1(email, password, name_first, name_last):
     return {
