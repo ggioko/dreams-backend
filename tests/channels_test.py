11276e8b991c4e718d79fc5527fd14d5e2bbd06e
Julius Vandeleur <z5205069@ad.unsw.edu.au>
2021-03-05 23:40:31 +1100

    Added test for user in multiple channels

--- tests/channels_test.py
+++ tests/channels_test.py
@@ -3,13 +3,8 @@ channels_test.py
 
 
 z5205069 Julius Vandeleur
-Tests for channnels_list()
+Tests for channnels_list_v1()
 
-Test 1: auth_user_id is invalid - throw AccessError (Section 6.3)
-
-Test 2: channels_list runs when auth_user_id = 1
-
-Test 3:  
 '''
 
 import pytest
@@ -51,22 +46,49 @@ def test_channels_list_one_channel():
     clear_v1() # Reset internal data to its initial state
     # Add new id to data set
     id = auth_register_v1('validemail@gmail.com', '123abc!@#', 'Hayden', 'Everest')
-    channel1_name = "My Channel" 
-    channel2_name = "Not My Channel"
-    channels_create_v1(1, channel1_name, True)
-    channels_create_v1(2, channel2_name, True)                  
-    channel_join_v1(id, channel1_name)
+    channel1Name = "My Channel" 
+    channel2Name = "Not My Channel"
+    channels_create_v1(1, channel1Name, True)
+    channels_create_v1(2, channel2Name, True)                  
+    channel_join_v1(id, channel1Name)
     assert channels_list_v1(id) == {
         'channels': [
         {
             'channel_id': 1,
             	'name': 'My Channel',
         }     
-        ],
-            
+        ],            
     }
     
+# Test for user that is in multiple channels
+# Output should be a dictionary {channels} with one set of data.
+def test_channels_list_multi_channels():
+    clear_v1() # Reset internal data to its initial state
+    # Add new id to data set
+    id = auth_register_v1('validemail@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    channel1Name = "Channel1" 
+    channel2Name = "Channel2"
+    channel3Name = "Channel3"
+    channel4Name = "Channel4"
+    # Create 4 channels, and join 2 of them
+    channels_create_v1(1, channel1Name, True)
+    channels_create_v1(2, channel2Name, True)
+    channels_create_v1(3, channel3Name, True)
+    channels_create_v1(4, channel4Name, True)                    
+    channel_join_v1(id, channel1Name)
+    channel_join_v1(id, channel3Name)
     
+    # Number of channels the user is found to be joined to.
+    channelCount = 0    
+    for channel in channels_list_v1(id):
+        if channel1Name == channel['name']:
+            ++channelCount
+        elif channel2Name == channel['name']:
+            ++channelCount
+            
+    # User should be in 2 channels.
+    assert channelCount == 2
+       
     
     
     
