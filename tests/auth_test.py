78cc9617419315cc604d56c02493f99e2aa5a77d
z5260252 <g.jayasekera@student.unsw.edu.au>
2021-03-05 13:52:48 +1100

    Function name fix

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -42,31 +42,27 @@ def test_auth_register_invalid_lastname():
 
 def test_auth_login_valid_single_user():
     clear_v1
-    auth_register('validemail@gmail.com', '123abc!@#', 'Hayden', 'Everest')
-    assert auth_login('validemail@gmail.com', '123abc!@#') == 1
+    auth_register_v1('validemail@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    assert auth_login_v1('validemail@gmail.com', '123abc!@#') == 1
 
 def test_auth_login_valid_multiple_users():
     clear_v1
-    auth_register('validemail@gmail.com', '123abc!@#', 'Hayden', 'Everest')
-    auth_register('another@gmail.com', 'anoth432$%^', 'Random', 'Stranger')
-    auth_register('randomguy@gmail.com', 'abcEZ!123', 'Haydena', 'Friend')
-    assert auth_login('another@gmail.com', 'anoth432$%^') == 2
-    assert auth_login('randomguy@gmail.com', 'abcEZ!123') == 3
+    auth_register_v1('validemail@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    auth_register_v1('another@gmail.com', 'anoth432$%^', 'Random', 'Stranger')
+    auth_register_v1('randomguy@gmail.com', 'abcEZ!123', 'Haydena', 'Friend')
+    assert auth_login_v1('another@gmail.com', 'anoth432$%^') == 2
+    assert auth_login_v1('randomguy@gmail.com', 'abcEZ!123') == 3
 
 def test_auth_login_unregistered_email():
     clear_v1
     auth_register_v1('registered@gmail.com', '123abc!@#', 'Hayden', 'Everest')
     with pytest.raises(InputError):
-        assert auth_login('unregistered@gmail.com', '123abc!@#')
-        assert auth_login('random@gmail.com', '123abc!@#')
+        assert auth_login_v1('unregistered@gmail.com', '123abc!@#')
+        assert auth_login_v1('random@gmail.com', '123abc!@#')
 
 def test_auth_login_invalid_password():
     clear_v1
-    auth_register('validemail@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    auth_register_v1('validemail@gmail.com', '123abc!@#', 'Hayden', 'Everest')
     with pytest.raises(InputError):
-        assert auth_login('validemail@gmail.com', 'abc')
-        assert auth_login('validemail@gmail.com', '123abc#@!')
-    clear_v1
-    with pytest.raises(InputError):
-        assert auth_register_v1('invalidlastname@gmail.com', 'abc123123', 'Everest', '')
-        assert auth_register_v1('invalidlastname2@gmail.com', 'abc123123', 'Everest', 'asdvsdwu8d2asdvsdwu8d2asdvsdwu8d2asdvsdwu8d2asdvsdwu8d2')
+        assert auth_login_v1('validemail@gmail.com', 'abc')
+        assert auth_login_v1('validemail@gmail.com', '123abc#@!')
