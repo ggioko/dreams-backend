213a22a5973c30f26bb7f563103bd47800d7f2ee
Julius Vandeleur <z5205069@ad.unsw.edu.au>
2021-03-05 22:06:23 +1100

    Added test_channels_list_one_channel()

--- tests/channels_test.py
+++ tests/channels_test.py
@@ -16,9 +16,11 @@ import pytest
 
 #from src.error import InputError
 from src.error import AccessError
-from src.channels import channels_list_v1
+from src.channels import channels_list_v1, channels_create_v1
+from src.channel import channel_join_v1
 from src.auth import auth_register_v1
 from src.other import clear_v1
+
 
 
 
@@ -32,12 +34,40 @@ def test_channels_list_runs():
     assert channels_list_v1(1)
 
 # Test for user that is not in any channel.
-# Output should be an empty dictionary.
+# Output should be an empty list.
 def test_channels_list_no_channels():
     clear_v1() # Reset internal data to its initial state
     # Add new id to data set
     id = auth_register_v1('validemail@gmail.com', '123abc!@#', 'Hayden', 'Everest')
     # Loop through all channels, make sure id is not found in the channels' data.
-    emptyDict = {}
-    assert channels_list_v1(id) == emptyDict
-                        
+    assert channels_list_v1(id) == {
+        'channels': [
+        ],
+    }
+                     
+# Test for user that is in one channel only.
+# Output should be a dictionary {channels} with one set of data.
+def test_channels_list_one_channel():
+    clear_v1() # Reset internal data to its initial state
+    # Add new id to data set
+    id = auth_register_v1('validemail@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    channel1_name = "My Channel" 
+    channel2_name = "Not My Channel"
+    channels_create_v1(1, channel1_name, True)
+    channels_create_v1(2, channel2_name, True)                  
+    channel_join_v1(id, channel1_name)
+    assert channels_list_v1(id) == {
+        'channels': [
+        {
+            'channel_id': 1,
+            	'name': 'My Channel',
+        }     
+        ],
+            
+    }
+    
+    
+    
+    
+    
+    
