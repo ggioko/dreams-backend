b4d4f8982af88828888c6ca494c22c6843031d5e
Sundeep Sarwana <z5208459@student.unsw.edu.au>
2021-03-05 14:10:38 +1100

    Wrote tests for channel_listall function

--- tests/channels_test.py
+++ tests/channels_test.py
@@ -1,9 +1,20 @@
 import pytest
 
-from src.channels import channels_create_v1
+from src.channels import channels_create_v1, channels_listall_v1, channels_create_v1
+from src.other import clear_v1
 from src.error import InputError
 
 def test_channels_create_invalid_channel_name():
     pass
+
+def test_channels_listall_runs():
+    assert channels_listall_v1(1)
+
+def test_channels_listall_check():
+    clear_v1()
+    name = "My Channel"
+    channels_create_v1(1,name,True)
+    channels = [channels_listall_v1(1)['channels'][c]['name'] for c in range(len(channels_listall_v1(1)['channels']))]
+    assert name in channels
 
 
