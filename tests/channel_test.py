8864d093e3be16ff7de4a19cd62cbf08ae3317c7
Ben Rayner <z5256104@ad.unsw.edu.au>
2021-03-06 18:07:50 +1100

    invalid channel test added

--- tests/channel_test.py
+++ tests/channel_test.py
@@ -1,11 +1,31 @@
 import pytest
 
+from src.auth import auth_register_v1
 from src.channel import channel_invite_v1
+from src.channels import channels_create_v1
 from src.echo import echo
 from src.error import InputError
+from src.other import clear_v1
+
+'''
+InputError when any of:
+channel_id does not refer to a valid channel.
+u_id does not refer to a valid user
+
+AccessError when any of:
+the authorised user is not already a member of the channel
+'''
 
 def test_channel_invite_invalid_channel():
-    pass
+    clear_v1()
+
+    adminID = auth_register_v1('madladadmin@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    userID = auth_register_v1('peasantuser@gmail.com', 'diffpassword!', 'Everest', 'Hayden')    
+    channelID = channels_create_v1(adminID, 'dankmemechannel', True)
+
+    with pytest.raises(InputError):
+        channel_invite_v1(adminID, channelID + 1, userID)
+
 
 def test_channel_invite_invalid_user():
     pass
