f1fa7ccaaf065ddef171597b9eaa8fb72ae94653
Julius Vandeleur <z5205069@ad.unsw.edu.au>
2021-03-07 05:41:16 +1100

    Added test_channel_details_multiple_members()

--- tests/channel_test.py
+++ tests/channel_test.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.channel import channel_details_v1, channel_join_v1
+from src.channel import channel_details_v1 #channel_join_v1
 from src.channels import channels_create_v1
 from src.auth import auth_register_v1
 from src.other import clear_v1
@@ -61,8 +61,32 @@ def test_channel_details_runs():
                 'handle_str': 'HaydenEverest',
             }
         ],
-    }     
-       
+    }  
+
+def test_channel_details_multiple_members():
+    clear_v1()
+    auth_register_v1('validemail0@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    auth_register_v1('validemail1@gmail.com', '123abc!@#', 'Fred', 'Smith')
+    auth_register_v1('validemail2@gmail.com', '123abc!@#', 'Jimmy', 'Barnes')
+    auth_register_v1('validemail3@gmail.com', '123abc!@#', 'Angus', 'Young')
+    
+    channels_create_v1(1, 'Channel1', True)
+#    channel_join_v1(2, 1)       
+#    channel_join_v1(3, 1)       # Commented out as requires channel_join_v1 to work
+#    channel_join_v1(4, 1)
+#    
+#    correct = 0
+#    if channel_details_v1(1,1)['name'] == 'Channel1':
+#        if len(channel_details_v1(1,1)['owner_members']) == 1:
+#            if len(channel_details_v1(1,1)['all_members']) == 4:
+#                correct = 1
+#           
+#    assert correct == 1
+    pass   
+        
+        
+        
+        
 def test_channel_messages_invalid_id():
     pass
 
