8922cfd4a9323cf5557ad3dbee10e0049a71fa9d
Julius Vandeleur <z5205069@ad.unsw.edu.au>
2021-03-06 23:38:14 +1100

    Currently failing 2 tests

--- tests/channels_test.py
+++ tests/channels_test.py
@@ -69,12 +69,15 @@ def test_channels_create_returns_integer():
 
 # Throw access error if auth_user_id is invalid (Section 6.3)
 def test_channels_list_access_error():
+    clear_v1()
     with pytest.raises(AccessError):
         # Pass a string into channels_list_v1 - should return access error.
         assert channels_list_v1('invalid')
         
 # Do we need this??
 def test_channels_list_runs():
+    clear_v1()
+    auth_register_v1('validemail@gmail.com', '123abc!@#', 'Hayden', 'Everest')
     assert channels_list_v1(1)
 
 # Test for user that is not in any channel.
@@ -82,9 +85,9 @@ def test_channels_list_runs():
 def test_channels_list_no_channels():
     clear_v1() # Reset internal data to its initial state
     # Add new id to data set
-    id = auth_register_v1('validemail@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    auth_register_v1('validemail@gmail.com', '123abc!@#', 'Hayden', 'Everest')
     # Loop through all channels, make sure id is not found in the channels' data.
-    assert channels_list_v1(id) == {
+    assert channels_list_v1(1) == {
         'channels': [
         ],
     }
@@ -94,13 +97,13 @@ def test_channels_list_no_channels():
 def test_channels_list_one_channel():
     clear_v1() # Reset internal data to its initial state
     # Add new id to data set
-    id = auth_register_v1('validemail@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    auth_register_v1('validemail@gmail.com', '123abc!@#', 'Hayden', 'Everest')
     channel1Name = "My Channel" 
     channel2Name = "Not My Channel"
     channels_create_v1(1, channel1Name, True)
-    channels_create_v1(2, channel2Name, True)                  
-    channel_join_v1(id, channel1Name)
-    assert channels_list_v1(id) == {
+    channels_create_v1(1, channel2Name, True)                  
+    channel_join_v1(1, channel1Name)
+    assert channels_list_v1(1) == {
         'channels': [
         {
             'channel_id': 1,
@@ -114,22 +117,22 @@ def test_channels_list_one_channel():
 def test_channels_list_multi_channels():
     clear_v1() # Reset internal data to its initial state
     # Add new id to data set
-    id = auth_register_v1('validemail@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    auth_register_v1('validemail@gmail.com', '123abc!@#', 'Hayden', 'Everest')
     channel1Name = "Channel1" 
     channel2Name = "Channel2"
     channel3Name = "Channel3"
     channel4Name = "Channel4"
     # Create 4 channels, and join 2 of them
     channels_create_v1(1, channel1Name, True)
-    channels_create_v1(2, channel2Name, True)
-    channels_create_v1(3, channel3Name, True)
-    channels_create_v1(4, channel4Name, True)                    
-    channel_join_v1(id, channel1Name)
-    channel_join_v1(id, channel3Name)
+    channels_create_v1(1, channel2Name, True)
+    channels_create_v1(1, channel3Name, True)
+    channels_create_v1(1, channel4Name, True)                    
+    channel_join_v1(1, channel1Name)
+    channel_join_v1(1, channel3Name)
     
     # Number of channels the user is found to be joined to.
     channelCount = 0    
-    for channel in channels_list_v1(id):
+    for channel in channels_list_v1(1):
         if channel1Name == channel['name']:
             ++channelCount
         elif channel2Name == channel['name']:
