1fb18a879f2cd288e644afb2d9af59be379c57c1
Gio Ko <z5310479@ad.unsw.edu.au>
2021-03-06 13:28:20 +1100

    updated channels.py

--- src/channels.py
+++ src/channels.py
@@ -1,4 +1,4 @@
-from src.error import InputError
+from src.error import InputError, AccessError
 from src.data import data
 
 def channels_list_v1(auth_user_id):
@@ -23,9 +23,17 @@ def channels_listall_v1(auth_user_id):
 
 def channels_create_v1(auth_user_id, name, is_public):
     
-    # Input error checking 
+    # Input error checking
+    # If length of name is more thant 20 characters raise InputError 
     if len(name) > 20:
         raise InputError("Error the channel name is more than 20 characters")
+    
+    # If is_public is not true or false raise InputError
+    if type(is_public) != bool:
+        raise InputError("Error the is_public value is not valid can only be True or False")
+
+    # Tracks whether auth_user_id has been matched
+    valid = 0
 
     # If user ids match, store details of user
     for user in data['users']:
@@ -38,6 +46,11 @@ def channels_create_v1(auth_user_id, name, is_public):
                     'handle' : user['handle_str'],
                     'password' : user['password']
             }
+            valid = 1
+
+    # If user id hasnt been matched, raise AccessError
+    if valid == 0:
+        raise AccessError("Error occurred auth_user_id is not valid")
     
     # Find the channel number, which is channel id
     channel_num = len(data['channels']) + 1
