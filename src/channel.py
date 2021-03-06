214e65650f80ce43c49e6904a9a4b9e62e2d3de7
z5260252 <g.jayasekera@student.unsw.edu.au>
2021-03-06 21:30:55 +1100

    Add a docstring to explain the channel_join_v1 function

--- src/channel.py
+++ src/channel.py
@@ -39,6 +39,23 @@ def channel_leave_v1(auth_user_id, channel_id):
     return {
     }
 
+"""
+Given a channel_id of a channel that the authorised user can join, adds them to that channel
+
+Arguments:
+    auth_user_id (integer)    - Users email
+    channel_id (integer)    - Users set password
+
+Exceptions:
+    InputError  - Occurs when channel_id is not a valid channel
+    AccessError  - Occurs when channel_id refers to a channel that is private (when
+                    the authorised user is not a global owner)
+
+Return Value:
+    Returns {}
+
+"""
+
 def channel_join_v1(auth_user_id, channel_id):
     return {
     }
