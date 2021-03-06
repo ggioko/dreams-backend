ee32b6dd38182fa8db18fd81fbb332e56b164739
Ben Rayner <z5256104@ad.unsw.edu.au>
2021-03-06 18:43:53 +1100

    other two test errors added

--- tests/channel_test.py
+++ tests/channel_test.py
@@ -4,7 +4,7 @@ from src.auth import auth_register_v1
 from src.channel import channel_invite_v1
 from src.channels import channels_create_v1
 from src.echo import echo
-from src.error import InputError
+from src.error import InputError, AccessError
 from src.other import clear_v1
 
 '''
@@ -28,10 +28,25 @@ def test_channel_invite_invalid_channel():
 
 
 def test_channel_invite_invalid_user():
-    pass
+    clear_v1()
+
+    adminID = auth_register_v1('madladadmin@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    userID = auth_register_v1('peasantuser@gmail.com', 'diffpassword!', 'Everest', 'Hayden')    
+    channelID = channels_create_v1(adminID, 'dankmemechannel', True)
+
+    with pytest.raises(InputError):
+        channel_invite_v1(adminID, channelID, userID + 1)
 
 def test_channel_invite_invalid_authoriser():
-    pass
+    clear_v1()
+
+    adminID = auth_register_v1('madladadmin@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    user1ID = auth_register_v1('peasantuser1@gmail.com', 'diffpassword!', 'Everest', 'Hayden')    
+    user2ID = auth_register_v1('peasantuser2@gmail.com', 'diffpassword.', 'Hayrest', 'Everden')
+    channelID = channels_create_v1(adminID, 'dankmemechannel', True)
+
+    with pytest.raises(AccessError):
+        channel_invite_v1(user1ID, channelID, user2ID)
 
 def test_channel_details_invalid_id():
     pass
