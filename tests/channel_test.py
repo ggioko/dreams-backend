84cac875a87ebce6588b6fee09e0099f9c5a588c
Julius Vandeleur <z5205069@ad.unsw.edu.au>
2021-03-07 02:12:24 +1100

    Added first 2 exception tests

--- tests/channel_test.py
+++ tests/channel_test.py
@@ -1,8 +1,10 @@
 import pytest
 
-from src.channel import channel_invite_v1
-from src.echo import echo
-from src.error import InputError
+from src.channel import channel_details_v1, channel_join_v1, channel_invite_v1
+from src.channels import channels_create_v1, channels_listall_v1, channels_list_v1
+from src.auth import auth_register_v1
+from src.other import clear_v1
+from src.error import InputError, AccessError
 
 def test_channel_invite_invalid_channel():
     pass
@@ -14,10 +16,19 @@ def test_channel_invite_invalid_authoriser():
     pass
 
 def test_channel_details_invalid_id():
-    pass
+    clear_v1()
+    auth_register_v1('validemail@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    with pytest.raises(AccessError):
+        assert channel_details_v1(1, 'invalidID')
+
 
 def test_channel_details_unauthorised_user():
-    pass
+    clear_v1() # Clear data and register 2 new users.
+    auth_register_v1('validemail@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    auth_register_v1('secondemail@gmail.com', '321cba#@!', 'Fred', 'Smith')
+    channels_create_v1(1, 'Channel1', True) # User 1 is a member because they created Channel1.
+    with pytest.raises(InputError):
+        assert channel_details_v1(2, 'Channel1')  # User 2 is not a member.      
 
 def test_channel_messages_invalid_id():
     pass
