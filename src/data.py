e87ca8a701b1c7bc2e9a0c3ebc9a46853d349599
Julius Vandeleur <z5205069@ad.unsw.edu.au>
2021-03-07 17:06:45 +1100

    Added contents of data.py back

--- src/data.py
+++ src/data.py
@@ -1,1 +1,55 @@
 # Data storage global variable
+data = {
+    'users': [
+        {
+            'u_id': 1,
+            'email': 'cs1531@cse.unsw.edu.au',
+            'password': 'abc1234',
+            'name_first': 'Hayden',
+            'name_last': 'Jacobs',
+            'handle_str': 'haydenjacobs',
+        },
+    ],
+    'channels': [
+        {
+            'id': 1,
+            'name' : 'channel1',
+            'is_public': True,
+            'owner_members': [
+                {
+                    'u_id': 1,
+                    'email': 'cs1531@cse.unsw.edu.au',
+                    'name_first': 'Hayden',
+                    'name_last': 'Jacobs',
+                    'handle_str': 'haydenjacobs',
+                },
+            ],
+            'all_members': [
+                {
+                    'u_id': 1,
+                    'email': 'cs1531@cse.unsw.edu.au',
+                    'name_first': 'Hayden',
+                    'name_last': 'Jacobs',
+                    'handle_str': 'haydenjacobs',
+                },
+            ],
+        },
+        {
+            'id': 2,
+            'name' : 'channel2',
+            'is_public': True,
+            'owner_members': [
+            ],
+            'all_members': [
+            ],
+            'messages' : [
+                {
+                    'message_id': 1,
+                    'u_id': 1,
+                    'message': 'Hello world',
+                    'time_created': 1582426789,
+                }
+        ],
+        },
+    ],
+}
