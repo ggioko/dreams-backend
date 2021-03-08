710d1f8acaea837d7711f8d3011289146f583eb6
Hayden Smith <mail@chambalam.com>
2021-03-09 01:42:34 +1100

    Iteration 2

---
+++ http_tests/echo_http_test.py
@@ -0,0 +1,11 @@
+import pytest
+import requests
+import json
+from src import config
+
+def test_echo():
+    '''
+    A simple test to check echo
+    '''
+    resp = requests.get(config.url + 'echo', params={'data': 'hello'})
+    assert json.loads(resp.text) == {'data': 'hello'}
