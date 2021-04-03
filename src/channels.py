fa7ac1a04515ce468c85c2d7c7ac85edabe89601
Gio Ko <z5310479@ad.unsw.edu.au>
2021-04-03 15:26:49 +1100

    implemented channels_create_v2

--- src/channels.py
+++ src/channels.py
@@ -1,6 +1,15 @@
-from src.error import InputError, AccessError
+from src.error import InputError
 from src.data import data
+import re
+import jwt
+import hashlib
 
+SECRET = 'dorito'
+
+def get_token_user_id(token):
+    decoded_jwt = jwt.decode(token, SECRET, algorithms=['HS256'])
+    u_id = int(decoded_jwt['u_id'])
+    return u_id
 
 '''
 channels_list_v1() - z5205069 Julius Vandeleur
@@ -98,6 +107,74 @@ def channels_create_v1(auth_user_id, name, is_public):
     # Input error also raised if is_public was not of type bool
     if type(is_public) != bool:
         raise InputError("Error the is_public value is not valid can only be True or False")
+
+    # Tracks whether auth_user_id has been matched
+    valid = 0
+
+    # If user ids match, store details of user
+    for user in data['users']:
+        if user['u_id'] == auth_user_id:
+            reuser = {
+                    'u_id' : user['u_id'],
+                    'email' : user['email'],
+                    'name_first' : user['name_first'],
+                    'name_last' : user['name_last'],
+                    'handle_str' : user['handle_str'],
+                    'password' : user['password']
+            }
+            # user id has been matched
+            valid = 1
+
+    # If user id hasnt been matched, raise AccessError
+    if valid == 0:
+        raise AccessError("Error occurred auth_user_id is not valid")
+    
+    # Find the channel number, which is channel id
+    channel_num = len(data['channels']) + 1
+
+    data['channels'].append({
+                    'id' : channel_num,
+                    'name' : name,
+                    'is_public' : is_public,
+                    'owner_members' : [reuser],
+                    'all_members' : [reuser],
+                    'messages': {}
+    })
+
+    # returns channel id
+    return {
+        'channel_id': channel_num,
+    }
+
+def channels_create_v2(token, name, is_public): 
+    """
+    Creates a new channel with that name that is either a public or private channel
+
+    Arguments:
+        token (string)    - Users id
+        name (string)    - Channel name
+        is_public (bool)    - Channel is public
+
+    Exceptions:
+        InputError - Occurs when the channel name is more than 20 characters, or
+                    when is_public is not given a value of type bool 
+        AccessError - Occurs when auth_user_id is not registered on the database,
+                    does not match any u_id
+
+    Return Value:
+        Returns {'channel_id': channel_num,} on success
+
+    """
+    # Input error checking
+    # If length of name is more thant 20 characters raise InputError 
+    if len(name) > 20:
+        raise InputError("Error the channel name is more than 20 characters")
+    
+    # Input error also raised if is_public was not of type bool
+    if type(is_public) != bool:
+        raise InputError("Error the is_public value is not valid can only be True or False")
+
+    auth_user_id = get_token_user_id(token)
 
     # Tracks whether auth_user_id has been matched
     valid = 0
