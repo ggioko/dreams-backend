75a31ca3186860a213fe2c9db97bc95cf155ae2e
z5260252 <g.jayasekera@student.unsw.edu.au>
2021-04-03 23:52:10 +1100

    fixed an issue with server not runnning properly

--- src/server.py
+++ src/server.py
@@ -6,7 +6,6 @@ from src.error import InputError
 from src import config
 from src.auth import auth_login_v2, auth_register_v2, auth_logout_v1
 from src.channels import channels_create_v2
-from src.channel import channel_join_v2
 from src.other import clear_v1
 
 def defaultHandler(err):
