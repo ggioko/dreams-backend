228f13e55dfcd89cb8664d579d36360630fdd6ae
Gio Ko <z5310479@ad.unsw.edu.au>
2021-04-03 16:51:36 +1100

    fixed token input of tests

--- tests/channels_test.py
+++ tests/channels_test.py
@@ -1,47 +1,49 @@
 import pytest
 from src.channels import channels_create_v2
-from src.auth import auth_register_v2
+from src.auth import auth_register_v2, get_token
 from src.other import clear_v1
 from src.error import InputError, AccessError
+from src.helper import generate_token
 
 from src.data import data
 
 # Test the case that Auth_user_id is invalid for channels create
 # Expected AccessError
-def test_channels_create_invalid_Auth_user_id():
+def test_channels_create_invalid_auth_user_id():
     clear_v1()
-    auth_register_v2('validemail@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    register1 = auth_register_v2('validemail@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    invalid_token = generate_token(4)
     with pytest.raises(AccessError):
-        assert channels_create_v1('invalid', 'invalid', True)
+        assert channels_create_v2(invalid_token, 'channel1', True)
 
 # Test the case where the channel name is more than 20 characters
 # Expected InputError
 def test_channels_create_invalid_channel_name():
     clear_v1()
-    auth_register_v2('validemail@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    register1 = auth_register_v2('validemail@gmail.com', '123abc!@#', 'Hayden', 'Everest')
     with pytest.raises(InputError):         
-        assert channels_create_v2(1, 'Nameismorethan20characters', True) 
+        assert channels_create_v2(register1['token'], 'Nameismorethan20characters', True) 
 
-# Test the case where is_public is of type bool
+# Test the case where is_public is not of type bool
 # Expected InputError
 def test_channels_create_invalid_is_public():
     clear_v1()
-    auth_register_v2('validemail@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    register1 = auth_register_v2('validemail@gmail.com', '123abc!@#', 'Hayden', 'Everest')
     with pytest.raises(InputError):         
-        assert channels_create_v2(1, 'channel', 20) 
+        assert channels_create_v2(register1['token'], 'channel', 20) 
 
 # Test the case where the channel name is less than 20 characters
 # Expected autotest pass
 def test_channels_create_valid_channel_name():
     clear_v1()
-    auth_register_v2('validemail@gmail.com', '123abc!@#', 'Hayden', 'Everest')
-    valid = channels_create_v2(1, 'channel', True)
+    register1 = auth_register_v2('validemail@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    valid = channels_create_v2(register1['token'], 'channel', True)
     assert valid['channel_id'] != None
 
 # Test whether the channel id returned by channels_create is an integer
 def test_channels_create_returns_integer():
     clear_v1()
-    auth_register_v2('validemail@gmail.com', '123abc!@#', 'Hayden', 'Everest')
-    new_channel = channels_create_v2(1, 'channel', True)
+    register1 = auth_register_v2('validemail@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    new_channel = channels_create_v2(register1['token'], 'channel', True)
     # Check if return type is an int
     assert isinstance(new_channel['channel_id'], int)
