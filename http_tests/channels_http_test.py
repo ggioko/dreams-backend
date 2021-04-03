6a576888cf6c0d786e84cbcc16e71ade14056448
Gio Ko <z5310479@ad.unsw.edu.au>
2021-04-03 21:12:01 +1100

    Added tests for errors

--- http_tests/channels_http_test.py
+++ http_tests/channels_http_test.py
@@ -1,11 +1,45 @@
 import pytest
 import requests
 import json
-from src import confc
+from src import config
+from src.helper import generate_token
 
-def test_create():
+def test_channels_create():
     """
     A simple test to check channels_create works by passing valid information
     """
-    resp = requests.get(config.url + 'channels_create', params={'token': 'token', 'name': 'channel1', 'is_public': True})
-    assert json.loads(resp.text) == {'channel_id': 'channel_id'}
+    # Clear data first
+    r = requests.delete(config.url + 'clear/v1')
+    r = requests.post(config.url + 'auth/register/v2', json={'email':'validemail@gmail.com',
+    'password' : '123abc!@#', 'name_first':'Hayden', 'name_last':'Everest'})
+    register1 = r.json()
+    r = requests.post(config.url + 'channels/create/v2',  json={'token': register1['token'], 'name': 'channel1', 'is_public': True})
+    assert r.json() == {'channel_id' : 1}
+
+def test_channels_create():
+    """
+    Testings for errors 
+    """
+    # Clear data first
+    r = requests.delete(config.url + 'clear/v1')
+    r = requests.post(config.url + 'auth/register/v2', json={'email':'validemail@gmail.com',
+    'password' : '123abc!@#', 'name_first':'Hayden', 'name_last':'Everest'})
+    register1 = r.json()
+
+    # Test the case where the channel name is more than 20 characters
+    # Expected bad request 400
+    r = requests.post(config.url + 'channels/create/v2',  json={'token': register1['token'], 'name': 'nameislonger20characters', 'is_public': True})
+    assert r.status_code == 400
+
+    # Test the case where is_public is not of type bool
+    # Expected bad request 400
+    r = requests.post(config.url + 'channels/create/v2',  json={'token': register1['token'], 'name': 'channel1', 'is_public': 20})
+    assert r.status_code == 400
+
+
+    # Test the case where the token is invalid
+    # Ezpected fobidden 403
+    invalid_token = generate_token(4)
+    r = requests.post(config.url + 'channels/create/v2',  json={'token': invalid_token, 'name': 'channel1', 'is_public': True})
+    assert r.status_code == 403
+
