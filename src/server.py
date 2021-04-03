9da5adbce256185e31c446e150df345d6d8de7ec
Gio Ko <z5310479@ad.unsw.edu.au>
2021-04-03 15:50:31 +1100

    implemented flask for channels_create_v2

--- src/server.py
+++ src/server.py
@@ -4,6 +4,7 @@ from flask import Flask, request
 from flask_cors import CORS
 from src.error import InputError
 from src import config
+from src.channels import channels_create_v2
 
 def defaultHandler(err):
     response = err.get_response()
@@ -31,6 +32,26 @@ def echo():
     return dumps({
         'data': data
     })
+
+@APP.route("/channels/create/v2", methods=['POST'])
+def channels_create():
+    """
+    Gets user data from http json and passes it to the
+    channels_register_v2 function
+
+    Returns { 'channel_id': channel_id,} on success
+
+    """
+    data = request.get_json()
+
+    token = data['token']
+    name = data['name']
+    is_public = bool(data['is_public'])
+
+    channel_id = channels_create_v2(token, name, is_public)
+
+    return dumps (channel_id)
+
 
 if __name__ == "__main__":
     APP.run(port=config.port) # Do not edit this port
