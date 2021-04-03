ad2141e7f843793c281bb9836eb6d6db23bc5725
z5260252 <g.jayasekera@student.unsw.edu.au>
2021-04-04 02:54:01 +1000

    created user_all_v1 in other.py

--- src/other.py
+++ src/other.py
@@ -1,4 +1,5 @@
 from src.data import data
+from src.helper import check_token_valid
 
 def clear_v1():
     """
@@ -6,6 +7,41 @@ def clear_v1():
     """
     data['users'].clear()
     data['channels'].clear()
+
+def users_all_v1(token):
+    '''
+    Function that returns a list of all users and their associated details
+    
+    Arguments:
+        token (string)    - token
+
+    Exceptions:
+        AccessError  - Occurs when token is invalid
+
+    Return Value:
+        Returns {users} on success
+    '''
+
+    # Check if token is valid using helper
+    if check_token_valid(token) == False:
+        raise AccessError(description='Invalid token')
+    
+    user_store = data['users']
+
+    user_dict = {'users':[]}
+
+    for user in user_store['users']:
+        user_dict['users'].append({
+            'u_id': user['u_id'], 
+            'email': user['email'],
+            'name_first': user['name_first'], 
+            'name_last': user['name_last'], 
+            'handle_str': user['handle_str'],
+        })
+
+    return user_dict
+
+
 
 def search_v1(auth_user_id, query_str):
     return {
