4a87e749cd2483278288eb8c62f101e717d0af36
z5260252 <g.jayasekera@student.unsw.edu.au>
2021-04-05 06:39:02 +1000

    added docstrings

--- src/message.py
+++ src/message.py
@@ -1,12 +1,42 @@
-def message_send_v1(auth_user_id, channel_id, message):
+def message_send_v2(token, channel_id, message):
     return {
         'message_id': 1,
     }
 
-def message_remove_v1(auth_user_id, message_id):
+def message_remove_v1(token, message_id):
+    """
+    Given a message_id for a message, this message is removed from the channel/DM
+
+    Arguments:
+        token (string)    - token
+        message_id (int)    - messages id
+
+    Exceptions:c
+        InputError  - Occurs when the message_id no longer exists
+        AccesError  - 
+
+    Return Value:
+        Returns {} - (empty dict) on success
+    """
     return {
     }
 
-def message_edit_v1(auth_user_id, message_id, message):
+def message_edit_v2(token, message_id, message):
+    """
+    Given a registered users' email and password and returns their `auth_user_id` value and 'token'
+
+    Arguments:
+        email (string)    - Users email
+        password (string)    - Users password
+
+    Exceptions:
+        InputError  - Occurs when email has an incorrect format, email is not
+                    registered or when the password does not match the given
+                    email
+
+    Return Value:
+        Returns {'auth_user_id': id,} on success
+        Returns {'token': token,} on success
+    """
     return {
     }
