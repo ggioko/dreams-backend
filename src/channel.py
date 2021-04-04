6c2b7edb4668808b69daef8ea113a92b83f7ce08
Ben Rayner <z5256104@ad.unsw.edu.au>
2021-04-04 14:44:43 +1000

    Removed v1 function, cleaned up test files

--- src/channel.py
+++ src/channel.py
@@ -1,59 +1,8 @@
-import jwt
-# jwt may be unneccessary
 from src.error import InputError, AccessError
 from src.data import data
 from src.helper import get_token_user_id, check_token_valid, SECRET
 import jwt
 import hashlib
-
-def channel_invite_v1(auth_user_id, channel_id, u_id):
-    '''
-    channel_invite adds a user to a channel when an existing channel
-    user invites them into the channel.
-
-    Arguments:
-        auth_user_id (int) - The ID of the existing member of the channel
-        channel_id (int)   - The ID of the channel that the new member is to join
-        u_id (int)         - The ID of the user new to the channel
-
-    Exceptions:
-        InputError  - Occurs when channel_id does not refer to a valid channel
-        InputError  - Occurs when u_id is not a valid user
-        AccessError - Occurs when auth_user_id is not a member of the channel
-
-    Return Value:
-        Returns an empty dictionary when exceptions are not raised
-    '''
-    foundChannel = {}
-    for channel in data['channels']:
-        if channel['id'] == channel_id:
-            foundChannel = channel
-            break
-        print(channel['id'])
-
-    if foundChannel == {}:
-        raise InputError('Invalid channel ID provided')
-
-    userMatch = False
-    for user in data['users']:
-        if user['u_id'] == u_id:
-            userMatch = True
-            break
-    if userMatch == False:
-        raise InputError('Member to add not a valid user')
-
-    userMatch = False
-    for user in channel['owner_members']:
-        if user['u_id'] == auth_user_id:
-            userMatch = True
-            break
-    if userMatch == False:
-        raise AccessError('Authorised user not a channel member')
-
-    channel_join_v1(u_id, channel_id)
-    
-    return {
-    }
 
 def channel_invite_v2(token, channel_id, u_id):
     '''
--- tests/channel_test.py
+++ tests/channel_test.py
@@ -1,7 +1,7 @@
 import pytest
 
 from src.auth import auth_register_v1, auth_register_v2, auth_logout_v1
-from src.channel import channel_invite_v1, channel_details_v1, channel_join_v1, channel_join_v2, channel_messages_v1, channel_invite_v2
+from src.channel import channel_details_v1, channel_join_v1, channel_join_v2, channel_messages_v1, channel_invite_v2
 from src.channels import channels_create_v1, channels_create_v2
 from src.helper import generate_token, get_token_user_id, SECRET
 from src.error import InputError, AccessError
