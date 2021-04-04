de78e2285fce250cccd807ef8f61e51b5f9401e1
Julius Vandeleur <z5205069@ad.unsw.edu.au>
2021-04-04 14:15:58 +1000

    In lines 16 and 36 of user_http_test, user = json.loads(r.text) was added instead of r.json()

--- http_tests/user_http_test.py
+++ http_tests/user_http_test.py
@@ -3,6 +3,7 @@ import requests
 import json
 from src import config
 from src.helper import generate_token
+from src.error import AccessError, InputError
 
 def test_user_profile():
     """
@@ -12,8 +13,8 @@ def test_user_profile():
     requests.delete(config.url + 'clear/v1')
     r = requests.post(config.url + 'auth/register/v2', json={'email':'validemail@gmail.com',
     'password' : '123abc!@#', 'name_first':'Hayden', 'name_last':'Everest'})
-    user = r.json()
-    r = requests.get(config.url + 'user/profile/v2', params={'token': user['token'], 'u_id': user['auth_user_id']})
+    user = json.loads(r.text)
+    r = requests.get(config.url + 'user/profile/v2', json={'token': user['token'], 'u_id': user['auth_user_id']})
     assert r.json() == {'user': {
                              'u_id': user['auth_user_id'],
                              'email': 'validemail@gmail.com',
@@ -32,9 +33,9 @@ def test_user_profile_errors():
     # Test for input error - invalid u_id
     r = requests.post(config.url + 'auth/register/v2', json={'email':'validemail@gmail.com',
     'password' : '123abc!@#', 'name_first':'Hayden', 'name_last':'Everest'})
-    user = r.json()
-    r = requests.get(config.url + 'user/profile/v2', params={'token': user['token'], 'u_id': 'invalid_u_id'})
-    assert r.status_code == 400
-    r = requests.get(config.url + 'user/profile/v2', params={'token': 'invalid_token', 'u_id': user['auth_user_id']})
-    assert r.status_code == 403
+    user = json.loads(r.text)
+    r = requests.get(config.url + 'user/profile/v2', json={'token': user['token'], 'u_id': 'invalid_u_id'})
+    assert r.status_code == InputError().code
+    r = requests.get(config.url + 'user/profile/v2', json={'token': 'invalid_token', 'u_id': user['auth_user_id']})
+    assert r.status_code == AccessError().code
  
