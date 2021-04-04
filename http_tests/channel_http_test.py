20b961945d03bd14c162ed48d595aa8e71989a82
Gio Ko <z5310479@ad.unsw.edu.au>
2021-04-04 13:36:17 +1000

    Fixed testing errors and added a simple test

--- http_tests/channel_http_test.py
+++ http_tests/channel_http_test.py
@@ -38,6 +38,23 @@ def test_channel_join_errors():
     r = requests.post(config.url + 'channel/join/v2', json = {'token': register1['token'], 'channel_id': 'channel4'})
     assert r.status_code == 400
 
+def test_channel_messages():
+    """
+    A simple test to see if channel_messages works by passing valid information
+    """
+    # Clear register first.
+    r = requests.delete(config.url + 'clear/v1')
+    r = requests.post(config.url + 'auth/register/v2', json={'email':'validemail@gmail.com',
+    'password' : '123abc!@#', 'name_first':'Hayden', 'name_last':'Everest'})
+    register1 = r.json()
+
+    r = requests.post(config.url + 'channels/create/v2', json={'token': register1['token'], 'name': 'Channel1', 'is_public': True})
+    channel1 = r.json()
+ 
+    r = requests.get(config.url + 'channel/messages/v2', params={'token': register1['token'], 'channel_id': channel1['channel_id'], 'start': 0})
+    assert r.status_code == 200
+    
+
 def test_channel_messages_errors():
     """
     Testings for input and access errors 
@@ -47,27 +64,28 @@ def test_channel_messages_errors():
     r = requests.post(config.url + 'auth/register/v2', json={'email':'validemail@gmail.com',
     'password' : '123abc!@#', 'name_first':'Hayden', 'name_last':'Everest'})
     register1 = r.json()
-
+    
     r = requests.post(config.url + 'auth/register/v2', json={'email':'validemail3@gmail.com',
     'password' : '123abc!@#', 'name_first':'Angus', 'name_last':'Young'})
     register2 = r.json()
 
-    r = requests.post(config.url + 'channels/create/v2', json = {'token': register1['token'], 'name': 'Channel1', 'is_public': True})
+    r = requests.post(config.url + 'channels/create/v2', json={'token': register1['token'], 'name': 'Channel1', 'is_public': True})
     channel1 = r.json()
 
     # Test the case where the token is invalid - Expected fobidden 403 (AccessError)
     invalid_token = generate_token(4)
-    r = requests.get(config.url + 'channel/messages/v2', json={'token': invalid_token, 'channel_id': channel1['channel_id'], 'start': 0})
+    r = requests.get(config.url + 'channel/messages/v2', params={'token': invalid_token, 'channel_id': channel1['channel_id'], 'start': 0})
+    print(r.status_code)
     assert r.status_code == 403
-
+    
     # Test the case where the registered user is not a member of the channel - Expected fobidden 403 (AccessError)
-    r = requests.get(config.url + 'channel/messages/v2', json={'token': register2['token'], 'channel_id': channel1['channel_id'], 'start': 0})
+    r = requests.get(config.url + 'channel/messages/v2', params={'token': register2['token'], 'channel_id': channel1['channel_id'], 'start': 0})
     assert r.status_code == 403
 
     # Test the case where the channel_id is not valid - Expected bad request 400 (InputError)
-    r = requests.get(config.url + 'channel/messages/v2', json={'token': register1['token'], 'channel_id': 20, 'start': 0})
+    r = requests.get(config.url + 'channel/messages/v2', params={'token': register1['token'], 'channel_id': 20, 'start': 0})
     assert r.status_code == 400
 
     # Test the case where the start is greater than the total number of messages in the channel - Expected bad request 400 (InputError)
-    r = requests.get(config.url + 'channel/messages/v2', json={'token': register1['token'], 'channel_id': channel1['channel_id'], 'start': 50})
-    assert r.status_code == 403
+    r = requests.get(config.url + 'channel/messages/v2', params={'token': register1['token'], 'channel_id': channel1['channel_id'], 'start': 50})
+    assert r.status_code == 400
