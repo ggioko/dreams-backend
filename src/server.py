72859c80546f10b8f67da403b64125b662fa7791
Gio Ko <z5310479@ad.unsw.edu.au>
2021-04-04 11:51:54 +1000

    fixed pylint issues

--- src/server.py
+++ src/server.py
@@ -6,7 +6,7 @@ from src.error import InputError
 from src import config
 from src.auth import auth_login_v2, auth_register_v2, auth_logout_v1
 from src.channels import channels_create_v2
-from src.channel import channel_join_v2
+from src.channel import channel_join_v2, channel_messages_v2
 from src.other import clear_v1
 
 def defaultHandler(err):
@@ -151,7 +151,7 @@ def channel_messages():
     channel_id = data['channel_id']
     start = data['start']
 
-    reponse = channel_messages_v2(token, channel_id, start)
+    response = channel_messages_v2(token, channel_id, start)
 
     return dumps(response)
     
