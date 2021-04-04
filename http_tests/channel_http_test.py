f57cec6324f8a1d3100893f4c09343c1d1361560
Julius Vandeleur <z5205069@ad.unsw.edu.au>
2021-04-04 23:19:19 +1000

    Fixed small typo in http tests for channel_details_v2

--- http_tests/channel_http_test.py
+++ http_tests/channel_http_test.py
@@ -24,9 +24,8 @@ def test_channel_details():
 #    r = requests.post(config.url + 'auth/register/v2', json = {'email':'validemail4@gmail.com', \
 #    'password' : '123abc!@#', 'name_first':'James', 'name_last':'Taylor'})
 #    rego_4 = r.json()
-    r = requests.post(config.url + 'channel/create/v2', json = {'token': rego_1['token'], 'name': 'Channel1', 'is_public': True})
+    r = requests.post(config.url + 'channels/create/v2', json = {'token': rego_1['token'], 'name': 'Channel1', 'is_public': True})
     new_channel = r.json()
-    assert new_channel == {'hello'}
     requests.post(config.url + 'channel/join/v2', json = {'token': rego_2['token'], 'channel_id': new_channel['channel_id']})
     requests.post(config.url + 'channel/join/v2', json = {'token': rego_3['token'], 'channel_id': new_channel['channel_id']})
     
@@ -61,7 +60,7 @@ def test_channel_details_errors():
     'password' : '123abc!@#', 'name_first':'James', 'name_last':'Taylor'})
     rego_4 = r.json()
     # Create new channel 'Channel1' using rego_1.
-    r = requests.post(config.url + 'channel/create/v2', json = {'token': rego_1['token'], 'name': 'Channel1', 'is_public': True})
+    r = requests.post(config.url + 'channels/create/v2', json = {'token': rego_1['token'], 'name': 'Channel1', 'is_public': True})
     new_channel = r.json()
     requests.post(config.url + 'channel/join/v2', json = {'token': rego_2['token'], 'channel_id': new_channel['channel_id']})
     requests.post(config.url + 'channel/join/v2', json = {'token': rego_3['token'], 'channel_id': new_channel['channel_id']})
