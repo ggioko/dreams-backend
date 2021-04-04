01388fefd9bc4360994263a21959c8f88e2da7f4
Julius Vandeleur <z5205069@ad.unsw.edu.au>
2021-04-04 12:40:43 +1000

    Changed 'params' to 'json' in http requests for both channels_list tests

--- http_tests/channels_http_test.py
+++ http_tests/channels_http_test.py
@@ -10,13 +10,13 @@ def test_channels_list():
     """
     # Clear register first.
     requests.delete(config.url + 'clear/v1')
-    r = requests.post(config.url + 'auth/register/v2', params = {'email':'validemail@gmail.com', \
+    r = requests.post(config.url + 'auth/register/v2', json = {'email':'validemail@gmail.com', \
     'password' : '123abc!@#', 'name_first':'Hayden', 'name_last':'Everest'})
     rego_1 = r.json()
-    requests.post(config.url + 'channels/create/v2', params = {'token': rego_1['token'], 'name': 'Channel1', 'is_public': True})
-    requests.post(config.url + 'channels/create/v2', params = {'token': rego_1['token'], 'name': 'Channel2', 'is_public': True})   
+    requests.post(config.url + 'channels/create/v2', json = {'token': rego_1['token'], 'name': 'Channel1', 'is_public': True})
+    requests.post(config.url + 'channels/create/v2', json = {'token': rego_1['token'], 'name': 'Channel2', 'is_public': True})   
     # Test for valid input
-    test_1 = requests.get(config.url + 'channels/list/v2', params = {'token': rego_1['token']})
+    test_1 = requests.get(config.url + 'channels/list/v2', json = {'token': rego_1['token']})
     payload_1 = test_1.json()
     
     # Number of channels the user is found to be joined to.
@@ -33,7 +33,7 @@ def test_channels_list_error():
     # Clear data first
     requests.delete(config.url + 'clear/v1')
     # invalid user - Access Error
-    test_2 = requests.get(config.url + 'channels/list/v2', params = {'token': 'invalid_user'})
+    test_2 = requests.get(config.url + 'channels/list/v2', json = {'token': 'invalid_user'})
     assert test_2.status_code == 403
     
     
