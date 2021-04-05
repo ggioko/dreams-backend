c6738c9211dd23ed34a3a7e7cf686f53d8c82011
Ben Rayner <z5256104@ad.unsw.edu.au>
2021-04-05 22:55:44 +1000

    Added preliminary admin_tests. Still need to implement adding privelidges in the first place without checking to be functional

---
+++ tests/admin_test.py
@@ -0,0 +1,50 @@
+import pytest
+
+from src.auth import auth_register_v2
+from src.admin import userpermission_change_v1
+from src.data import data
+
+from src.error import InputError, AccessError
+from src.other import clear_v1
+
+def test_admin_userpermission_change_success():
+    '''
+    Tests userpermission_change_v1() with all correct information
+    '''
+    user1 = auth_register_v2('validemail@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    user2 = auth_register_v2('validemail2@gmail.com', '1234abc!@#', 'Haydenn', 'Everestt')
+    
+    # Call helper function to make user1 dreams owner
+
+    assert userpermission_change_v1(user1['token'], user2['u_id'], 1) == {}
+
+def test_admin_userpermission_change_invalid_user():
+    '''
+    Tests userpermission_change_v1() with an invalid dreams ownership change
+    '''
+    user1 = auth_register_v2('validemail@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    
+    # Call helper function to make user1 dreams owner
+
+    with pytest.raises(InputError):
+        userpermission_change_v1(user1['token'], user1['u_id'] + 1, 1)
+
+def test_admin_userpermission_change_invalid_permission():
+    '''
+    Tests userpermission_change_v1() with an invalid new dreams permission
+    '''
+    user1 = auth_register_v2('validemail@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    user2 = auth_register_v2('validemail2@gmail.com', '1234abc!@#', 'Haydenn', 'Everestt')
+
+    # Call helper function to make user1 dreams owner
+
+    assert userpermission_change_v1(user1['token'], user2['u_id'], 100) == {}
+
+def test_admin_userpermission_change_nonowner_request():
+    '''
+    Tests userpermission_change_v1() with the request being added by a nonowner of **dreams**
+    '''
+    user1 = auth_register_v2('validemail@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    user2 = auth_register_v2('validemail2@gmail.com', '1234abc!@#', 'Haydenn', 'Everestt')
+
+    assert userpermission_change_v1(user1['token'], user2['u_id'], 1) == {}
