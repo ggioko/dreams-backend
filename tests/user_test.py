d4ab2ff55d31f9d1ebe12ae11312243d83be47f3
Julius Vandeleur <z5205069@ad.unsw.edu.au>
2021-04-05 15:58:24 +1000

    Added tests and http tests for setname function

--- tests/user_test.py
+++ tests/user_test.py
@@ -4,7 +4,7 @@ from src.error import InputError, AccessError
 #from src.data import data
 from src.auth import auth_register_v2
 from src.other import clear_v1
-from src.user import users_all_v1, user_profile_v2, user_profile_setemail_v2
+from src.user import users_all_v1, user_profile_v2, user_profile_setemail_v2, user_profile_setname_v2
 from src.helper import generate_token
 
 def test_users_all_v1_successful():
@@ -114,3 +114,52 @@ def test_setemail_invalid_token():
     auth_register_v2('validemail0@gmail.com', '123abc!@#', 'Hayden', 'Everest')
     with pytest.raises(AccessError):                        
         assert user_profile_setemail_v2('invalid_token', 'newemail@gmail.com')
+
+def test_setname():
+    """
+    Pass in a user with valid token and new name.
+    The name in their data store should be replaced by the new name passed in.
+    """
+    clear_v1()
+    user = auth_register_v2('validemail0@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    user_profile_setname_v2(user['token'], 'Fred', 'Smith')
+    user_info  = user_profile_v2(user['token'], user['auth_user_id'])
+    assert user_info == {'user': {
+                             'u_id': user['auth_user_id'],
+                             'email': 'validemail0@gmail.com',
+                             'name_first': 'Fred',
+                             'name_last': 'Smith',
+                             'handle_str': 'HaydenEverest'          
+    }}
+    
+def test_setname_invalid_token():
+    """
+    Pass in a user with invalid token and valid new name.
+    Should return AccessError
+    """
+    clear_v1()
+    auth_register_v2('validemail0@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    with pytest.raises(AccessError):                        
+        assert user_profile_setname_v2('invalid_token', 'Fred', 'Smith')
+
+def test_setname_invalid_firstname():
+    """
+    Pass in a user with valid token and but invalid first name.
+    Should return AccessError
+    """
+    clear_v1()
+    user = auth_register_v2('validemail0@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    with pytest.raises(InputError):                        
+        assert user_profile_setname_v2(user['token'], 'asdvsdwu8d2asdvsdwu8d2asdvsdwu8d2asdvsdwu8d2asdvsdwu8d2', 'Smith')
+        
+def test_setname_invalid_lastname():
+    """
+    Pass in a user with valid token but invalid last name.
+    Should return AccessError
+    """
+    clear_v1()
+    user = auth_register_v2('validemail0@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    with pytest.raises(InputError):                        
+        assert user_profile_setname_v2(user['token'], 'Fred', 'asdvsdwu8d2asdvsdwu8d2asdvsdwu8d2asdvsdwu8d2asdvsdwu8d2')
+        
+
