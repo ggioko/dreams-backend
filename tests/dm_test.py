a9ea49fd671f2d9d96e4cc61aae2d18815046cca
Gio Ko <z5310479@ad.unsw.edu.au>
2021-04-05 22:16:01 +1000

    Added docstrings and removed helper functions

--- tests/dm_test.py
+++ tests/dm_test.py
@@ -3,20 +3,21 @@ from src.error import InputError, AccessError
 from src.dm import dm_create_v1, dm_details_v1, dm_leave_v1
 from src.auth import auth_register_v2
 from src.other import clear_v1
-from src.helper import get_token_user_id, generate_token
 
-# Testing when valid infomation is passes throught dm_leave
-# expected success
 def test_dm_details():
+    """
+    Testing when valid infomation is passed through dm_details
+    Expected: success
+    """
     # clear data and register members
     clear_v1()
     user_1 = auth_register_v2('validemail0@gmail.com', '123abc!@#', 'Hayden', 'Everest')
     user_2 = auth_register_v2('validemail1@gmail.com', '123abc!@#', 'Fred', 'Smith')
     user_3 = auth_register_v2('validemail2@gmail.com', '123abc!@#', 'Jimmy', 'Barnes')
 
-    # get user id from token using helper function
-    u_id1 = get_token_user_id(user_2['token'])
-    u_id2 = get_token_user_id(user_3['token'])
+    # get user ids
+    u_id1 = user_2['auth_user_id']
+    u_id2 = user_3['auth_user_id']
 
     # create a new DM
     dm_1 = dm_create_v1(user_1['token'], [u_id1, u_id2])
@@ -26,40 +27,44 @@ def test_dm_details():
                                         {'u_id': 2, 'email': 'validemail1@gmail.com', 'name_first': 'Fred', 'name_last': 'Smith', 'handle_str': 'fredsmith', },
                                         {'u_id': 3, 'email': 'validemail2@gmail.com', 'name_first': 'Jimmy', 'name_last': 'Barnes', 'handle_str': 'jimmybarnes',}]
 
-# Testing when an invalid token is used
-# expected AccessError
 def test_dm_details_invalid_token():
+    """
+    Testing when an invalid token is used
+    Expected: AccessError
+    """
     # clear data and register members
     clear_v1()
     user_1 = auth_register_v2('validemail0@gmail.com', '123abc!@#', 'Hayden', 'Everest')
     user_2 = auth_register_v2('validemail1@gmail.com', '123abc!@#', 'Fred', 'Smith')
     user_3 = auth_register_v2('validemail2@gmail.com', '123abc!@#', 'Jimmy', 'Barnes')
 
-    # get user id from token using helper function
-    u_id1 = get_token_user_id(user_2['token'])
-    u_id2 = get_token_user_id(user_3['token'])
+    # get user ids
+    u_id1 = user_2['auth_user_id']
+    u_id2 = user_3['auth_user_id']
 
     # create a new DM
     dm_1 = dm_create_v1(user_1['token'], [u_id1, u_id2])
-    
-    # generate invalid token using helper function
-    invalid_token = generate_token(4)
 
     with pytest.raises(AccessError):
-        assert dm_details_v1(invalid_token, dm_1['dm_id'])
+        assert dm_details_v1(-1, dm_1['dm_id'])
 
-# Testing when an invalid DM ID is used
-# expected InputError
 def test_dm_details_invalid_dm_id():
+    """
+    Testing when an invalid DM ID is used
+    Expected: InputError
+    """
     clear_v1()
     user_1 = auth_register_v2('validemail0@gmail.com', '123abc!@#', 'Hayden', 'Everest')
 
     with pytest.raises(InputError):
         assert dm_details_v1(user_1['token'], 5)
 
-# Testing when an authorised id is not a member of DM
-# expected AcessError
 def test_dm_details_invalid_authorised_member():
+    """
+    Testing when an authorised id is not a member of DM
+    Expected: AcessError
+    """
+
     # clear data and register members
     clear_v1()
     user_1 = auth_register_v2('validemail0@gmail.com', '123abc!@#', 'Hayden', 'Everest')
@@ -67,9 +72,9 @@ def test_dm_details_invalid_authorised_member():
     user_3 = auth_register_v2('validemail2@gmail.com', '123abc!@#', 'Jimmy', 'Barnes')
     user_4 = auth_register_v2('validemail3@gmail.com', '123abc!@#', 'Bob', 'Jones')
 
-    # get user id from token using helper function
-    u_id1 = get_token_user_id(user_2['token'])
-    u_id2 = get_token_user_id(user_3['token'])
+    # get user ids
+    u_id1 = user_2['auth_user_id']
+    u_id2 = user_3['auth_user_id']
 
     # create a new DM
     dm_1 = dm_create_v1(user_1['token'], [u_id1, u_id2])
@@ -77,43 +82,58 @@ def test_dm_details_invalid_authorised_member():
     with pytest.raises(AccessError):
         assert dm_details_v1(user_4['token'], dm_1['dm_id'])
 
-# Testing when an invalid token is used
-# expected AccessError
 def test_dm_create_invalid_token():
-    clear_v1()
-    user_1 = auth_register_v2('validemail@gmail.com', '123abc!@#', 'Hayden', 'Everest')
-    user_2 = auth_register_v2('secondemail@gmail.com', '321cba#@!', 'Fred', 'Smith')
-    u_id1 = get_token_user_id(user_1['token'])
-    u_id2 = get_token_user_id(user_2['token'])       
-    invalid_token = generate_token(4)
-    with pytest.raises(AccessError):
-        assert dm_create_v1(invalid_token, [u_id1, u_id2])
+    """
+    Testing when an invalid token is used
+    Expected: AccessError
+    """
 
-# Testing when an u_id does not refer to a valid member
-# expected InputError
-def test_dm_create_invalid_u_id():
+    # clear data and register members
     clear_v1()
     user_1 = auth_register_v2('validemail@gmail.com', '123abc!@#', 'Hayden', 'Everest')
     user_2 = auth_register_v2('secondemail@gmail.com', '321cba#@!', 'Fred', 'Smith')
-    u_id1 = get_token_user_id(user_2['token'])      
+    u_id1 = user_1['auth_user_id']
+    u_id2 = user_2['auth_user_id'] 
+    with pytest.raises(AccessError):
+        assert dm_create_v1(-1, [u_id1, u_id2])
+
+def test_dm_create_invalid_u_id():
+    """
+    Testing for when an u_id does not refer to a valid member
+    Expected: InputError
+    """
+
+    # clear data and register members
+    clear_v1()
+    user_1 = auth_register_v2('validemail@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    user_2 = auth_register_v2('secondemail@gmail.com', '321cba#@!', 'Fred', 'Smith')
+    u_id1 = user_2['auth_user_id']
     u_id2 = 4
     with pytest.raises(InputError):
         assert dm_create_v1(user_1['token'], [u_id1, u_id2])
 
-# Testing when creating a DM create is successful
 def test_dm_create_success():
+    """
+    Testing when valid information passed through dm_create
+    Expected: success
+    """
+
+    # clear data and register members
     clear_v1()
     user_1 = auth_register_v2('validemail@gmail.com', '123abc!@#', 'Hayden', 'Everest')
     user_2 = auth_register_v2('secondemail@gmail.com', '321cba#@!', 'Fred', 'Smith')
     user_3 = auth_register_v2('thirdemail@gmail.com', '321bca#@!', 'Bob', 'Jones')
-    u_id1 = get_token_user_id(user_2['token'])
-    u_id2 = get_token_user_id(user_3['token'])
+    u_id1 = user_2['auth_user_id']
+    u_id2 = user_3['auth_user_id']
     new_dm = dm_create_v1(user_1['token'], [u_id1, u_id2])
     assert new_dm['dm_id'] == 1
     assert new_dm['dm_name'] == "bobjones, fredsmith, haydeneverest"       
 
-# Testing dm_remove for error handling
 def test_dm_leave_errors():
+    """
+    Testing dm_leave for errors
+    """
+
     # Clear data
     clear_v1()
     # Register members and setup data
@@ -124,23 +144,26 @@ def test_dm_leave_errors():
     new_dm = dm_create_v1(user_1['token'], [u_id1])
 
     # Testing when an invalid token is used
-    # expected AccessError
+    # Expected: AccessError
     with pytest.raises(AccessError):
         assert dm_leave_v1(-1, new_dm['dm_id'])
 
     # Testing when an invalid DM ID is used
-    # expected InputError
+    # Expected: InputError
     with pytest.raises(InputError):
         assert dm_leave_v1(user_1['token'], 10)
 
     # Testing when an authorised id is not a member of DM
-    # expected AcessError
+    # Expected: AcessError
     with pytest.raises(AccessError):
         assert dm_leave_v1(user_3['token'], new_dm['dm_id'])
 
-# Testing when valid infomation is passes through dm_leave
-# expected success
 def test_dm_leave():
+    """
+    Testing when valid infomation is passes through dm_leave
+    Expected: success
+    """
+
     # Clear data
     clear_v1()
     # Register members and setup data
