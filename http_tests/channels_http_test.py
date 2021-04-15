1e75ff500cb10dfc5c57a89f8c876c304fa6a5db
Sundeep Sarwana <z5208459@student.unsw.edu.au>
2021-04-15 16:59:00 +1000

    Chnaged one more get method from json to params

--- http_tests/channels_http_test.py
+++ http_tests/channels_http_test.py
@@ -110,5 +110,5 @@ def test_channels_list_error():
     # Clear data first
     requests.delete(config.url + 'clear/v1')
     # invalid user - Access Error
-    test_2 = requests.get(config.url + 'channels/list/v2', json = {'token': 'invalid_user'})
+    test_2 = requests.get(config.url + 'channels/list/v2', params = {'token': 'invalid_user'})
     assert test_2.status_code == AccessError().code
