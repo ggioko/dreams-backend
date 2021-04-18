8300a9f11b3e40f9fabfd7859041eae908be996e
Gajendra Jayasekera <g.jayasekera@student.unsw.edu.au>
2021-04-18 12:14:51 +1000

    Revert "Merge branch 'message_unpin_v1' into 'master'"
    This reverts merge request !77

--- src/data.py
+++ src/data.py
@@ -39,44 +39,11 @@ data = {
                     #'u_id': 1,
                     #'message': 'Hello world',
                     #'time_created': 1582426789,
-                    #'is_pinned' : Boolean,
                 },
             ],
         },
     ],
-    'dms' : [
-        {
-            #'dm_id': 1,
-            #'name' : 'user_handles',
-            'owner_members': [
-                {
-                    #'u_id': 1,
-                    #'email': 'cs1531@cse.unsw.edu.au',
-                    #'name_first': 'Hayden',
-                    #'name_last': 'Jacobs',
-                    #'handle_str': 'haydenjacobs',
-                },
-            ],
-            'all_members': [
-                {
-                    #'u_id': 1,
-                    #'email': 'cs1531@cse.unsw.edu.au',
-                    #'name_first': 'Hayden',
-                    #'name_last': 'Jacobs',
-                    #'handle_str': 'haydenjacobs',
-                },
-            ],
-            'messages' : [
-                {
-                    #'message_id': 1,
-                    #'u_id': 1,
-                    #'message': 'Hello world',
-                    #'time_created': 1582426789,
-                    #'is_pinned' : Boolean,
-                },
-            ],
-        },
-    ],
+    'dms' : [],
     'active_tokens' : [],
     'message_count' : 0,
 }
