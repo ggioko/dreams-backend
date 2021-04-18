77018b0e4462a42d372d2d62e09063c4d2987194
Ben Rayner <z5256104@ad.unsw.edu.au>
2021-04-18 22:30:42 +1000

    Finished initial regular pytests

--- tests/other_test.py
+++ tests/other_test.py
@@ -6,21 +6,7 @@ from src.channels import channels_create_v2
 from src.channel import channel_messages_v2, channel_invite_v2
 from src.auth import auth_register_v2, auth_logout_v1
 from src.message import message_send_v2, message_senddm_v1
-
-
-"""
-Given a query string, return a collection of messages in all of the channels/DMs
-that the user has joined that match the query
-Arguments:
-    token (str)        - The token of the user who wants to search their messages
-    query_str (str)    - The string that the user wants to find matches of
-
-Exceptions:
-    InputError  - Occurs when the length of the query string is over 1000 characters
-
-Return Value:
-    Returns a dictionary of messages in channels and DMs that the user is a part of
-"""
+from src.dm import dm_create_v1,
 
 def test_search_channel_messages_v2_valid():
     """
@@ -35,13 +21,88 @@ def test_search_channel_messages_v2_valid():
     channel_invite_v2(user1['token'], channel_1['channel_id'], user2['auth_user_id'])
 
 
-    message = "hello this is my new channel"
-    message2 = "lennahc wen ym si siht olleh"
+    messagecontent1 = "hello this is my new channel"
+    messagecontent2 = "lennahc wen ym si siht olleh"
 
-    message_1 = message_send_v2(user1['token'], channel_1['channel_id'], message)
-    message_2 = message_send_v2(user1['token'], channel_1['channel_id'], message)
+    message_1 = message_send_v2(user1['token'], channel_1['channel_id'], messagecontent1)
+    message_2 = message_send_v2(user1['token'], channel_1['channel_id'], messagecontent2)
     
     matching = search_v2(user1['token'], "my new")
     assert matching['messages'][0]['message_id'] == message_1['message_id']
 
 
+def test_search_channel_v2_too_long_input():
+    """
+    Checks if input error is raised with a character input that is too long in
+    search_v2()
+    """
+    clear_v1()
+
+    user1 = auth_register_v2('madladadmin@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    channel_1 = channels_create_v2(user1['token'], 'dankmemechannel', False)
+    
+    with pytest.raises(InputError):
+        search_v2(user1['token'], "a" * 1100)
+
+def test_search_channel_messages_v2_not_in_channel():
+    """
+    Checks if user can't see messages when they're not in the channel
+    with search_v2()
+    """
+    clear_v1()
+
+    user1 = auth_register_v2('madladadmin@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    user2 = auth_register_v2('peasantuser@gmail.com', 'diffpassword!', 'Everest', 'Hayden')    
+    channel_1 = channels_create_v2(user1['token'], 'dankmemechannel', False)
+
+    messagecontent1 = "hello this is my new channel"
+    messagecontent2 = "lennahc wen ym si siht olleh"
+
+    message_1 = message_send_v2(user1['token'], channel_1['channel_id'], messagecontent1)
+    message_2 = message_send_v2(user1['token'], channel_1['channel_id'], messagecontent2)
+    
+    matching = search_v2(user2['token'], "my new")
+    assert matching['messages'] == []
+
+def test_search_dms_v2_valid():
+    """
+    Checks if correct messages retuned with valid parameters when searching
+    with search_v2() for DMs
+    """
+    clear_v1()
+
+    user1 = auth_register_v2('madladadmin@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    user2 = auth_register_v2('peasantuser@gmail.com', 'diffpassword!', 'Everest', 'Hayden')    
+
+    messagecontent1 = "hello this is my new channel"
+    messagecontent2 = "lennahc wen ym si siht olleh"
+
+    new_dm = dm_create_v1(user1['token'], [user2['auth_user_id']])
+
+    dm_1 = message_send_v2(user1['token'], new_dm['dm_id'], messagecontent1)
+    dm_2 = message_send_v2(user1['token'], new_dm['dm_id'], messagecontent2)
+    
+    matching = search_v2(user1['token'], "my new")
+    assert matching['messages'][0]['message_id'] == message_1['message_id']
+
+def test_search_dms_v2_invaliduser():
+    """
+    Checks if correct messages retuned with matches not from the user's dms
+    with search_v2() for DMs
+    """
+    clear_v1()
+
+    user1 = auth_register_v2('madladadmin@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    user2 = auth_register_v2('peasantuser@gmail.com', 'diffpassword!', 'Everest', 'Hayden')    
+    user3 = auth_register_v2('thirdemail@gmail.com', '321bca#@!', 'Bob', 'Jones')
+
+    messagecontent1 = "hello this is my new channel"
+    messagecontent2 = "lennahc wen ym si siht olleh"
+
+    new_dm = dm_create_v1(user1['token'], [user_2['auth_user_id']])
+
+    dm_1 = message_send_v2(user1['token'], new_dm['dm_id'], messagecontent1)
+    dm_2 = message_send_v2(user1['token'], new_dm['dm_id'], messagecontent2)
+    
+    matching = search_v2(user3['token'], "my new")
+    assert matching['messages'] == []
