dfbbe4c3faf4b7dddfeed7102aa0b763f4603ada
Gio Ko <z5310479@ad.unsw.edu.au>
2021-04-19 21:49:47 +1000

    Added initial tests for standup_send

--- tests/standup_test.py
+++ tests/standup_test.py
@@ -5,7 +5,7 @@ from src.error import InputError, AccessError
 from src.other import clear_v1
 from src.auth import auth_register_v2
 from src.channels import channels_create_v2
-from src.standup import standup_start_v1, standup_active_v1
+from src.standup import standup_start_v1, standup_active_v1, standup_send_v1
 
 def test_standup_start_invalid_channel_id():
     """
@@ -37,7 +37,7 @@ def test_standup_start_invalid_authorised_member():
 def test_standup_start_invalid_token():
     """
     Testing for when the token is invalid
-    Expected: InputError
+    Expected: AccessError
     """
 
     # clear data and register members
@@ -125,3 +125,75 @@ def test_standup_active_finish():
     standup_start_v1(user_1['token'], channel_1['channel_id'], 0)
 
     assert standup_active_v1(user_1['token'], channel_1['channel_id']) == {'is_active' : False, 'time_finish' : None}
+
+def test_standup_send_invalid_token():
+    """
+    Testing for when the token is invalid
+    Expected: AccessError
+    """
+    # clear data and register members
+    clear_v1()
+    user_1 = auth_register_v2('validemail0@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    channel_1 = channels_create_v2(user_1['token'], 'channel1', True)
+
+    invalid_token = -1
+
+    with pytest.raises(AccessError):
+        assert standup_send_v1(invalid_token, channel_1['channel_id'], 'hello')
+
+def test_standup_send_invalid_channel_id():
+    """
+    Testing for when the channel id is invalid
+    Expected: InputError
+    """
+    # clear data and register members
+    clear_v1()
+    user_1 = auth_register_v2('validemail0@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+
+    invalid_channel_id = 20
+
+    with pytest.raises(InputError):
+        assert standup_send_v1(user_1['token'], invalid_channel_id, 'hello')
+    
+def test_standup_send_invalid_message():
+    """
+    Testing for when the message is longer than 1000 characters
+    Expected: InputError
+    """
+    # clear data and register members
+    clear_v1()
+    user_1 = auth_register_v2('validemail0@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    channel_1 = channels_create_v2(user_1['token'], 'channel1', True)
+
+    with pytest.raises(InputError):
+        assert standup_send_v1(user_1['token'], channel_1['channel_id'], "a"*1100)
+
+def test_standup_send_authorised_user():
+    """
+    Testing for when a authorised user is not a member of channel
+    Expected: AccessError
+    """
+    # clear data and register members
+    clear_v1()
+    user_1 = auth_register_v2('validemail0@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    user_2 = auth_register_v2('validemail1@gmail.com', '123abc!@#', 'Fred', 'Smith')
+    channel_1 = channels_create_v2(user_1['token'], 'channel1', True)
+
+    with pytest.raises(AccessError):
+        assert standup_send_v1(user_2['token'], channel_1['channel_id'], "hello")
+
+def test_standup_send_active_not_running():
+    """
+    Testing for when a active standup is running in channel
+    Expected: InputError
+    """
+    # clear data and register members
+    clear_v1()
+    user_1 = auth_register_v2('validemail0@gmail.com', '123abc!@#', 'Hayden', 'Everest')
+    channel_1 = channels_create_v2(user_1['token'], 'channel1', True)
+
+    standup_start_v1(user_1['token'], channel_1['channel_id'], 0)
+    standup_active_v1(user_1['token'], channel_1['channel_id'])
+
+    with pytest.raises(InputError):
+        assert standup_send_v1(user_1['token'], channel_1['channel_id'], "hello")
