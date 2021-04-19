8f4c4fe7ae92030f70e23e93e49af8fc6facc5bf
Julius Vandeleur <z5205069@ad.unsw.edu.au>
2021-04-20 03:32:30 +1000

    Saving latest work to branch

---
+++ src/notifications.py
@@ -0,0 +1,16 @@
+from src.helper import get_token_user_id
+from src.data import data
+
+
+def notifications_get_v1(token):
+    u_id = get_token_user_id(token)
+    
+    for user in data['users']:
+        if user['u_id'] == u_id:
+            notifs_list = user['notifications']
+            
+        if len(notifs_list) >= 20:
+            notifications = notifs_list[::-1][0:20]
+        else:
+            notifications = notifs_list[::-1]
+        return notifications
